a76a69d3b01a03484711a3dacf2f1d23e9c1a067
Шаймухаметов Ильдар <shaimildar@gmail.com>
2022-06-20 00:18:10 +0500

    reformat

--- CRM.App/ClientApp/src/setupProxy.js
+++ CRM.App/ClientApp/src/setupProxy.js
@@ -1,26 +1,29 @@
-const createProxyMiddleware = require('http-proxy-middleware');
-const { env } = require('process');
+const createProxyMiddleware = require("http-proxy-middleware");
+const { env } = require("process");
 
-const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_HTTPS_PORT}` :
-  env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'http://localhost:44419';
+const target = env.ASPNETCORE_HTTPS_PORT
+  ? `https://localhost:${env.ASPNETCORE_HTTPS_PORT}`
+  : env.ASPNETCORE_URLS
+  ? env.ASPNETCORE_URLS.split(";")[0]
+  : "http://localhost:44419";
 
-const context =  [
+const context = [
   "/weatherforecast",
   "/_configuration",
   "/.well-known",
   "/Identity",
   "/connect",
   "/ApplyDatabaseMigrations",
-  "/_framework"
+  "/_framework",
 ];
 
-module.exports = function(app) {
+module.exports = function (app) {
   const appProxy = createProxyMiddleware(context, {
     target: target,
     secure: false,
     headers: {
-      Connection: 'Keep-Alive'
-    }
+      Connection: "Keep-Alive",
+    },
   });
 
   app.use(appProxy);
--- CRM.App/ClientApp/src/setupTests.js
+++ CRM.App/ClientApp/src/setupTests.js
@@ -8,16 +8,17 @@ global.localStorage = localStorageMock;
 
 // Mock the request issued by the react app to get the client configuration parameters.
 window.fetch = () => {
-  return Promise.resolve(
-    {
-      ok: true,
-      json: () => Promise.resolve({
-        "authority": "https://localhost:7119",
-        "client_id": "CRM.App",
-        "redirect_uri": "https://localhost:7119/authentication/login-callback",
-        "post_logout_redirect_uri": "https://localhost:7119/authentication/logout-callback",
-        "response_type": "id_token token",
-        "scope": "CRM.AppAPI openid profile"
-     })
-    });
+  return Promise.resolve({
+    ok: true,
+    json: () =>
+      Promise.resolve({
+        authority: "https://localhost:7119",
+        client_id: "CRM.App",
+        redirect_uri: "https://localhost:7119/authentication/login-callback",
+        post_logout_redirect_uri:
+          "https://localhost:7119/authentication/logout-callback",
+        response_type: "id_token token",
+        scope: "CRM.AppAPI openid profile",
+      }),
+  });
 };
