d216f3819405ccfd5897a6fc962fe8473e8df786
Шаймухаметов Ильдар <shaimildar@gmail.com>
2022-06-29 17:53:56 +0500

    remove comment

--- CRM.App/ClientApp/src/utils/hooks.js
+++ CRM.App/ClientApp/src/utils/hooks.js
@@ -12,11 +12,6 @@ function useSafeDispatch(dispatch) {
   );
 }
 
-// Example usage:
-// const {data, error, status, run} = useAsync()
-// React.useEffect(() => {
-//   run(fetchPokemon(pokemonName))
-// }, [pokemonName, run])
 const defaultInitialState = { status: "idle", data: null, error: null };
 function useAsync(initialState) {
   const initialStateRef = React.useRef({
