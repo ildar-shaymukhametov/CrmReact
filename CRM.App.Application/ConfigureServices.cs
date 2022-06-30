5594e2a1bf6787051bfc9085a321d9008e4a7ad2
Шаймухаметов Ильдар <shaimildar@gmail.com>
2022-06-30 20:56:36 +0500

    add application project

--- CRM.App.Application/ConfigureServices.cs
+++ CRM.App.Application/ConfigureServices.cs
@@ -1,4 +1,5 @@
 ﻿using System.Reflection;
+using CRM.App.Application.Common.Behaviours;
 using FluentValidation;
 using MediatR;
 
@@ -11,6 +12,10 @@ public static class ConfigureServices
         services.AddAutoMapper(Assembly.GetExecutingAssembly());
         services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
         services.AddMediatR(Assembly.GetExecutingAssembly());
+        services.AddTransient(typeof(IPipelineBehavior<,>), typeof(UnhandledExceptionBehaviour<,>));
+        services.AddTransient(typeof(IPipelineBehavior<,>), typeof(AuthorizationBehaviour<,>));
+        services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehaviour<,>));
+        services.AddTransient(typeof(IPipelineBehavior<,>), typeof(PerformanceBehaviour<,>));
 
         return services;
     }
