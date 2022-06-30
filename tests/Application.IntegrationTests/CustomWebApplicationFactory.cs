1936ce592d517590c6ad24db4929065cd9f94f75
Шаймухаметов Ильдар <shaimildar@gmail.com>
2022-06-30 23:16:27 +0500

    add test projects

---
+++ tests/Application.IntegrationTests/CustomWebApplicationFactory.cs
@@ -0,0 +1,37 @@
+﻿using CRM.App.Application.Common.Interfaces;
+using CRM.App.Infrastructure.Persistence;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using NSubstitute;
+
+namespace CRM.App.Application.IntegrationTests;
+
+public class CustomWebApplicationFactory : WebApplicationFactory<Program>
+{
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.ConfigureAppConfiguration(configurationBuilder =>
+        {
+            var integrationConfig = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .AddEnvironmentVariables()
+                .Build();
+
+            configurationBuilder.AddConfiguration(integrationConfig);
+        });
+
+        builder.ConfigureServices((builder, services) =>
+        {
+            services.Remove<ICurrentUserService>()
+                .AddTransient(provider => Substitute.For<ICurrentUserService>().UserId.Returns(BaseTestFixture.GetCurrentUserId()));
+
+            services.Remove<DbContextOptions<ApplicationDbContext>>()
+                .AddDbContext<ApplicationDbContext>((sp, options) =>
+                    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"),
+                        builder => builder.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName)));
+        });
+    }
+}
---
+++ tests/Application.UnitTests/Mappings/MappingTests.cs
@@ -0,0 +1,32 @@
+﻿using System.Runtime.Serialization;
+using AutoMapper;
+using CRM.App.Application.Common.Mappings;
+
+namespace CRM.App.Application.UnitTests.Mappings;
+
+public class MappingTests
+{
+    private readonly IConfigurationProvider _configuration;
+    private readonly IMapper _mapper;
+
+    public MappingTests()
+    {
+        _configuration = new MapperConfiguration(config => config.AddProfile<MappingProfile>());
+        _mapper = _configuration.CreateMapper();
+    }
+
+    [Fact]
+    public void ShouldHaveValidConfiguration()
+    {
+        _configuration.AssertConfigurationIsValid();
+    }
+
+    private object GetInstanceOf(Type type)
+    {
+        if (type.GetConstructor(Type.EmptyTypes) != null)
+            return Activator.CreateInstance(type)!;
+
+        // Type without parameterless constructor
+        return FormatterServices.GetUninitializedObject(type);
+    }
+}
