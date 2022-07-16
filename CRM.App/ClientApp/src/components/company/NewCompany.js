baf37946b99df7a10a805a5afb25218e98264184
Шаймухаметов Ильдар <shaimildar@gmail.com>
2022-07-16 20:58:02 +0500

    add breadcrumbs to NewCompany

--- CRM.App/ClientApp/src/components/company/NewCompany.js
+++ CRM.App/ClientApp/src/components/company/NewCompany.js
@@ -1,10 +1,11 @@
 import React from "react";
-import { Alert, Container } from "reactstrap";
+import { Alert, Breadcrumb, BreadcrumbItem, Container } from "reactstrap";
 import { CompanyForm } from "./CompanyForm";
 import { useAsync } from "../../utils/hooks";
 import { useCreateCompany } from "../../utils/companies";
 import { Navigate } from "react-router-dom";
 import { AppRoutes } from "../../AppConstants";
+import { Link } from "react-router-dom";
 
 export { NewCompany };
 
@@ -26,6 +27,14 @@ function NewCompany() {
 
   return (
     <Container>
+      <div>
+        <Breadcrumb>
+          <BreadcrumbItem>
+            <Link to={AppRoutes.Companies}>Companies</Link>
+          </BreadcrumbItem>
+          <BreadcrumbItem active>New</BreadcrumbItem>
+        </Breadcrumb>
+      </div>
       <h5>Create a new company</h5>
       {isError ? (
         <Alert color="danger">Failed to create a new company: {error}</Alert>
